79d7f9b99cd51fc4e7244cad28c858e0d30cfc5f
Yujiro Akihiro <yujiro.akihiro@gmail.com>
2024-07-30 05:23:18 +0200

    #2 2nd Push

--- routes/drones.js
+++ routes/drones.js
@@ -1,21 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
+// import Drone.model
+const Drone = require('../models/Drone.model');
+
 // require the Drone model here
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
-  // ... your code here
+  Drone.find()
+    .then(allDrones => {
+      res.render('drones/list', { drones: allDrones });
+    })
+
+    .catch(err => {
+      console.log('Error retrieving drones:', err);
+      next(err);
+    });
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  res.render('drones/create-form');
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
-  // ... your code here
+  const { name, propellers, maxSpeed } = req.body;
+
+  Drone.create({ name, propellers, maxSpeed })
+    .then(() => res.redirect('/drones'))
+    .catch(err => res.render('drones/create-form'));
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
